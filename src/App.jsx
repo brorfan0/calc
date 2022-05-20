229059f4c1070d48764570475d52ecef5f58fd1e
anna <anna@pop-os.localdomain>
2022-05-20 16:48:43 +0200

    lilac alerts

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import {useEffect, useState} from 'react'
 import './App.css'
 import Button from "./Button";
+import {toast, ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
@@ -26,29 +28,29 @@ function App() {
     //TODO make it possible to nest brackets gcfhchfchfchhxhreh
 
 
-    useEffect(()=>{
-        if(ifDelete === true){
+    useEffect(() => {
+        if (ifDelete === true) {
             setIfDelete(false);
-        }else{
-            if(openingBrackets === closingBrackets && brackets === 0){
+        } else {
+            if (openingBrackets === closingBrackets && brackets === 0) {
                 setInsideBrackets("");
-            }else{
+            } else {
                 setInsideBrackets(insideBrackets + calculations.slice(calculations.length - 1));
             }
         }
-    },[calculations]);
+    }, [calculations]);
 
-    useEffect(()=>{
-        if(equation > 0){
+    useEffect(() => {
+        if (equation > 0) {
             setSign("neg");
-        }else if(equation < 0){
+        } else if (equation < 0) {
             setSign("pos");
         }
         setEquation(equation.toString());
     }, [equation]);
 
-    useEffect(()=>{
-        switch(ifNum){
+    useEffect(() => {
+        switch (ifNum) {
             case "not":
                 setNumbs("");
                 setEmpty(true);
@@ -56,116 +58,115 @@ function App() {
                 break;
             default:
                 const added = calculations.slice(-1);
-                if(numbs === "" && dot === true){
+                if (numbs === "" && dot === true) {
                     setNumbs("0" + added);
-                }else{
+                } else {
                     setNumbs(numbs + added);
                 }
         }
     }, [ifNum]);
 
-    function handleSndClick(e){
-        switch(ifNum){
+    function handleSndClick(e) {
+        switch (ifNum) {
             case "not":
-                equation === "" ? alert("can't do this") : (setCalculations(equation+e), setEquation(""));
+                equation === "" ? toast.error("Can't do this") : (setCalculations(equation + e), setEquation(""));
                 break;
             default:
-                if(calculations===""){
-                    alert("can't do this");
-                }else{
+                if (calculations === "") {
+                    toast.error("Can't do this");
+                } else {
                     setIfNum("not");
                     setSign("neg");
-                    setCalculations(calculations+e);
+                    setCalculations(calculations + e);
                 }
         }
-        if(brackets === 2){
+        if (brackets === 2) {
             setBrackets(0);
         }
     }
 
-    function handleClick(e){
-        if(calculations.slice(calculations.length-1) === ")"){
-            alert("Can't use a number straight after the closing bracket");
-        }else{
-            switch(e){
+    function handleClick(e) {
+        if (calculations.slice(calculations.length - 1) === ")") {
+            toast.error("Can't use a number straight after the closing bracket");
+        } else {
+            switch (e) {
                 case ".":
-                    if(dot === false){
+                    if (dot === false) {
                         empty ? setCalculations(calculations + "0" + e) : setCalculations(calculations + e);
                         setDot(true);
-                        setIfNum(ifNum+"num");
+                        setIfNum(ifNum + "num");
                         setEmpty(false);
-                    }else{
-                        alert("can't use dot twice in the same number");
+                    } else {
+                        toast.error("Can't use dot twice in the same number");
                     }
                     break;
                 default:
-                    setIfNum(ifNum+"num");
+                    setIfNum(ifNum + "num");
                     setEmpty(false);
 
-                    if(equation === "") {
-                        setCalculations(calculations+e);
-                    }
-                    else{
-                        setCalculations(calculations+e);
+                    if (equation === "") {
+                        setCalculations(calculations + e);
+                    } else {
+                        setCalculations(calculations + e);
                         setEquation("");
                     }
                     break;
             }
         }
-        if(brackets === 2){
+        if (brackets === 2) {
             setBrackets(0);
         }
         // setCalculations((prevState) => {return prevState+e})
     }
 
-    function calculate(){
-        if(equation === "" && calculations!== ""){
-                    setEquation(eval(calculations));
-                    setCalculations("");
-                    setBrackets(0);
-                }
+    function calculate() {
+        if (equation === "" && calculations !== "") {
+            setEquation(eval(calculations));
+            setCalculations("");
+            setBrackets(0);
+        }
         setIfNum("not");
     }
 
-    function output(){
-        if(equation === ""){
-            return(
+    function output() {
+        if (equation === "") {
+            return (
                 <p>{calculations}</p>
             )
-        }else{
-            return(
+        } else {
+            return (
                 <p>{equation}</p>
             )
         }
     }
 
-    function del(){
+    function del() {
         //TODO all of this is a mess but idk how else to do it help
         setIfDelete(true);
-        if(equation !== ""){
+        if (equation !== "") {
             setEquation(equation.slice(0, -1));
-        }else{
-            if(numbs.slice(numbs.length -1) === "."){
+        } else {
+            if (numbs.slice(numbs.length - 1) === ".") {
                 setDot(false);
             }
-            if(calculations.slice(calculations.length -1) === "("){
+            if (calculations.slice(calculations.length - 1) === "(") {
                 setBrackets(0);
-                setOpeningBrackets(openingBrackets-1);
+                setOpeningBrackets(openingBrackets - 1);
             }
-            if(nonNumbers.includes(calculations.charAt(calculations.length-2)) === true){
+            if (nonNumbers.includes(calculations.charAt(calculations.length - 2)) === true) {
                 setEmpty(true);
             }
-            if(nonNumbers.includes(calculations.slice(calculations.length - 1)) === true){
+            if (nonNumbers.includes(calculations.slice(calculations.length - 1)) === true) {
                 setIfNum("num")
             }
-            if(calculations.slice(calculations.length -1) === ")"){
+            if (calculations.slice(calculations.length - 1) === ")") {
                 setBrackets(1);
-                setClosingBrackets(closingBrackets-1);
+                setClosingBrackets(closingBrackets - 1);
             }
-            if(calculations.charAt(calculations.length-2) === ""){
+            if (calculations.charAt(calculations.length - 2) === "") {
                 setEmpty(true);
             }
-            if(openingBrackets > closingBrackets){
+            if (openingBrackets > closingBrackets) {
                 setInsideBrackets(insideBrackets.slice(0, -1));
             }
             setCalculations(calculations.slice(0, -1));
@@ -173,7 +174,7 @@ function App() {
         }
     }
 
-    function clear(){
+    function clear() {
         setCalculations("");
         setDot(false);
         setEmpty(true);
@@ -186,25 +187,25 @@ function App() {
         setClosingBrackets(0);
     }
 
-    function addBrackets(){
-        empty ? (setCalculations(calculations + "("), setBrackets(1), setOpeningBrackets(openingBrackets+1)) : alert("Can't do this");
+    function addBrackets() {
+        empty ? (setCalculations(calculations + "("), setBrackets(1), setOpeningBrackets(openingBrackets + 1)) : toast.error("Can't do this");
     }
 
-    function closeBrackets(){
-        if(openingBrackets < closingBrackets+1){
-            alert("Can't do this");
-        }else{
+    function closeBrackets() {
+        if (openingBrackets < closingBrackets + 1) {
+            toast.error("Can't close brackets without opening them");
+        } else {
             setCalculations(calculations + ")");
             setBrackets(2);
-            setClosingBrackets(closingBrackets+1)
+            setClosingBrackets(closingBrackets + 1)
         }
     }
 
-    function pow(){
-        if(equation !== ""){
+    function pow() {
+        if (equation !== "") {
             const power = equation + "*" + equation;
             setEquation(eval(power));
-        }else{
+        } else {
             const power = brackets === 2 ? (eval(insideBrackets) + "*" + eval(insideBrackets)) : (numbs + "*" + numbs);
             const toPower = eval(power);
             brackets === 2 ? (setCalculations(calculations.slice(0, -insideBrackets.length) + toPower), setBrackets(0))
@@ -213,24 +214,24 @@ function App() {
         }
     }
 
-    function minOrPlus(){
-        if(numbs === "0" || numbs === "" || equation === "0" || nonNumbers.includes(calculations.slice(calculations.length-1)) === true ||
-            calculations.slice(calculations.length-1) === "(" || calculations.slice(calculations.length-1) === ")"){
-            alert("Can only change signs on a number (different than zero)");
-        }else{
+    function minOrPlus() {
+        if (numbs === "0" || numbs === "" || equation === "0" || nonNumbers.includes(calculations.slice(calculations.length - 1)) === true ||
+            calculations.slice(calculations.length - 1) === "(" || calculations.slice(calculations.length - 1) === ")") {
+            toast.error("Can only change signs on a number (different than zero)");
+        } else {
             if (numbs === "" && equation !== "") {
-                    switch(sign){
-                        case "neg":
-                            setEquation("-" + equation);
-                            setSign("pos");
-                            break;
-                        case "pos":
-                            setEquation(equation.slice(1));
-                            setSign("neg");
-                            break;
-                    }
-            }else{
-                switch(sign){
+                switch (sign) {
+                    case "neg":
+                        setEquation("-" + equation);
+                        setSign("pos");
+                        break;
+                    case "pos":
+                        setEquation(equation.slice(1));
+                        setSign("neg");
+                        break;
+                }
+            } else {
+                switch (sign) {
                     case "neg":
                         setCalculations(calculations.slice(0, -numbs.length) + "(-" + numbs + ")");
                         setNumbs("(-" + numbs + ")");
@@ -249,6 +250,15 @@ function App() {
 
     return (
         <div className="App">
+            <ToastContainer
+                position="top-center"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                draggable={false}
+                pauseOnHover
+            />
             <div className="cont">
                 <div className="calc">{output()}</div>
                 <Button clas="buttonNorm" text="1" click={() => handleClick(1)}/>
@@ -270,7 +280,15 @@ function App() {
                 <Button clas="buttonNorm" text="9" click={() => handleClick(9)}/>
                 <Button clas="buttonNorm" text="-" click={() => handleSndClick('-')}/>
                 <Button clas="buttonNorm" text="^2" click={() => pow()}/>
-                <Button clas="buttonNorm" text=":)" click={() => alert(":)")}/>
+                <Button clas="buttonNorm" text=":)" click={() => toast('🦄 :)', {
+                    position: "top-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined
+                })}/>
                 <br/>
                 <Button clas="buttonNorm" text="0" click={() => handleClick(0)}/>
                 <Button clas="buttonNorm" text="." click={() => handleClick('.')}/>
